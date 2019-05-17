70bce52431bd406a0b5b526955fea26d222a483c
unoctium1 <unoctium1@gmail.com>
2019-05-16 17:55:53 -0700

    Added feature for pulling tours from webserver, also namespaced code

--- Assets/Scripts/TourBuilderScriptable.cs
+++ Assets/Scripts/TourBuilderScriptable.cs
@@ -4,87 +4,98 @@ using UnityEditor;
 using UnityEngine;
 using VRTour.Serialize;
 
-/// <summary>
-/// Scriptable object for building a tour. Needs a prefab for tour, node and answers, and loads a set tour
-/// </summary>
-[CreateAssetMenu(fileName = "Tour", menuName = "Build Tour", order = 1)]
-public class TourBuilderScriptable : ScriptableObject
+namespace VRTour
 {
-    public GameObject tourPrefab;
-    public GameObject nodePrefab;
-    public GameObject answerPrefab;
-
-    public IDictionary<int, NodeBehaviour> nodes;
-
-    public Tour toBuild;
-
-    private GameObject tourObj;
-
     /// <summary>
-    /// Loads the given tour
+    /// Scriptable object for building a tour. Needs a prefab for tour, node and answers, and loads a set tour
     /// </summary>
-    /// <param name="t">Tour to be build</param>
-    public void LoadTour(Tour t)
+    [CreateAssetMenu(fileName = "Tour", menuName = "Build Tour", order = 1)]
+    public class TourBuilderScriptable : ScriptableObject
     {
-        toBuild = t;
+        public GameObject tourPrefab;
+        public GameObject nodePrefab;
+        public GameObject answerPrefab;
+
+        public IDictionary<int, NodeBehaviour> nodes;
+
+        public Tour toBuild;
+
+        private GameObject tourObj;
+
+        /// <summary>
+        /// Resets to empty tour
+        /// </summary>
+        public void ResetTour()
+        {
+            toBuild = new Tour();
+        }
+
+        /// <summary>
+        /// Loads the given tour
+        /// </summary>
+        /// <param name="t">Tour to be build</param>
+        public void LoadTour(Tour t)
+        {
+            toBuild = t;
+        }
+
+        /// <summary>
+        /// Builds the tour in the scene
+        /// </summary>
+        public void BuildTour()
+        {
+            nodes = new Dictionary<int, NodeBehaviour>();
+            SetupTour();
+
+        }
+
+        /// <summary>
+        /// Recursively builds each node in the tour, under the parent TourPrefab object
+        /// </summary>
+        private void SetupTour()
+        {
+            tourObj = Instantiate(tourPrefab);
+            TourBehaviour tour = tourObj.GetComponent<TourBehaviour>();
+            tour.Setup(toBuild, this);
+            nodes = new Dictionary<int, NodeBehaviour>();
+            BuildNode(toBuild.startPoint);
+        }
+
+        /// <summary>
+        /// Takes a deserialized node and sets it up in the scene
+        /// </summary>
+        /// <param name="toBuild">The Deserialized node structure to setup</param>
+        /// <returns>the fully setup and built node object</returns>
+        public NodeBehaviour BuildNode(Node toBuild)
+        {
+            GameObject nodeObj = Instantiate(nodePrefab, tourObj.transform);
+            NodeBehaviour toReturn = nodeObj.GetComponent<NodeBehaviour>();
+            //Constructs the node based on the deserialized node in toBuild and adds it to nodes
+            toReturn.Setup(toBuild, this);
+            return toReturn;
+
+        }
+
+        /// <summary>
+        /// Takes a deserialized destination and sets up an answer button for it
+        /// </summary>
+        /// <param name="d">Deserialized destnation to setup</param>
+        /// <param name="t">The transform to build the answer into</param>
+        /// <returns></returns>
+        public AnswerBehaviour BuildAnswer(Destination d, RectTransform t)
+        {
+            GameObject ansObj = Instantiate(answerPrefab, t);
+            AnswerBehaviour toReturn = ansObj.GetComponent<AnswerBehaviour>();
+            toReturn.Setup(d, this);
+
+            return toReturn;
+
+        }
+        /*
+        public void SetupGM(GameManager toSetup)
+        {
+            toSetup.Setup(nodes, toBuild.startPoint.nodeId);
+        }
+        */
     }
-
-    /// <summary>
-    /// Builds the tour in the scene
-    /// </summary>
-    public void BuildTour()
-    {
-        nodes = new Dictionary<int, NodeBehaviour>();
-        SetupTour();
-
-    }
-
-    /// <summary>
-    /// Recursively builds each node in the tour, under the parent TourPrefab object
-    /// </summary>
-    private void SetupTour()
-    {
-        tourObj = Instantiate(tourPrefab);
-        TourBehaviour tour = tourObj.GetComponent<TourBehaviour>();
-        tour.Setup(toBuild, this);
-        nodes = new Dictionary<int, NodeBehaviour>();
-        BuildNode(toBuild.startPoint);
-    }
-
-    /// <summary>
-    /// Takes a deserialized node and sets it up in the scene
-    /// </summary>
-    /// <param name="toBuild">The Deserialized node structure to setup</param>
-    /// <returns>the fully setup and built node object</returns>
-    public NodeBehaviour BuildNode(Node toBuild)
-    {
-        GameObject nodeObj = Instantiate(nodePrefab, tourObj.transform);
-        NodeBehaviour toReturn = nodeObj.GetComponent<NodeBehaviour>();
-        //Constructs the node based on the deserialized node in toBuild and adds it to nodes
-        toReturn.Setup(toBuild, this);
-        return toReturn;
-
-    }
-
-    /// <summary>
-    /// Takes a deserialized destination and sets up an answer button for it
-    /// </summary>
-    /// <param name="d">Deserialized destnation to setup</param>
-    /// <param name="t">The transform to build the answer into</param>
-    /// <returns></returns>
-    public AnswerBehaviour BuildAnswer(Destination d, RectTransform t)
-    {
-        GameObject ansObj = Instantiate(answerPrefab, t);
-        AnswerBehaviour toReturn = ansObj.GetComponent<AnswerBehaviour>();
-        toReturn.Setup(d, this);
-
-        return toReturn;
-
-    }
-    /*
-    public void SetupGM(GameManager toSetup)
-    {
-        toSetup.Setup(nodes, toBuild.startPoint.nodeId);
-    }
-    */
 }
